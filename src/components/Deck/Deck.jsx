ff4229e88199e290586d90bba16d1fbc7dfff137
Marcia Tejeda <marcia.tejeda@unq.edu.ar>
2018-04-10 16:47:07 -0300

    deck css

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -6,10 +6,13 @@ const Deck = ({
     quantity
   }) => (
     <div className="deck">
+    <div className="text"> Mazo </div>
+    <div className="deck">
       <div className="number"><b>{quantity}</b></div>
       
       <img className="image" src={require('./inte.jpg')} />
 
+    </div>
     </div>
   )
   
