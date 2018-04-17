d439e6f8c9257a8cc429b8fa3866470e79f7bd49
Marcia Tejeda <marcia.tejeda@unq.edu.ar>
2018-04-17 17:50:36 -0300

    WIP: reduxing

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Timeline</h1>
-        </header>
+        
         <p className="App-intro">
           <Game />
         </p>
