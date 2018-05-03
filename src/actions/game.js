0460d76c9406886d4f141ed525364bc4c30a97f6
marcia-t <tejedamarcia@gmail.com>
2018-05-03 10:39:16 -0300

    WIP: adding winner to state

--- src/actions/game.js
+++ src/actions/game.js
@@ -31,6 +31,8 @@ export const cardPlacedInTimeline = (previousCard) => (dispatch,getState) => {
   let newDeck = deck
   let newDiscard = discard
   let newPlayerHand = player.playerHand.slice()
+  let ended = false
+  let winner = null
 
   const year = selectedCard.fact.year
   const prevYear = previousCard ? parseInt(previousCard.fact.year) : Number.NEGATIVE_INFINITY
@@ -41,6 +43,10 @@ export const cardPlacedInTimeline = (previousCard) => (dispatch,getState) => {
   newPlayerHand = newPlayerHand.filter( c => c.fact.name !== selectedCard.fact.name)
   if (prevYear < year && nextYear > year) {
     newTimeline = insert(prevCardIndex + 1, selectedCard, newTimeline)
+    if (newPlayerHand.length <1){
+      ended = true
+      winner = player
+    }
   }else {
     newPlayerHand.push(deck[0])
     newDeck = newDeck.slice(1,newDeck.length)
@@ -53,5 +59,7 @@ export const cardPlacedInTimeline = (previousCard) => (dispatch,getState) => {
     deck: newDeck,
     discard: newDiscard,
     playerHand: newPlayerHand,
+    winner: winner,
+    ended: ended
   }))
 }
