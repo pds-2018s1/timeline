0478fd24dee7e424eee2d737488491b44bd78842
LucasLando <lucaslando1@gmail.com>
2018-05-09 22:24:08 -0300

    Adding some css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import CreateMatch from '../CreateMatch/CreateMatch'
 import Matches from '../Matches/Matches'
 import CardsManagement from '../CardsManagement/CardsManagement'
+import './Home.css'
 
 const Home = ({name}) => (
-  <div>
-    <div>Hola {name}</div>
-    <CreateMatch />
-    <Matches />
-    <CardsManagement />
+  <div className="home">
+    <div className="greet">Hola {name}</div>
+    <div className="sectionContainer">
+      <div className="section"><CreateMatch /></div>
+      <div className="section"><Matches /></div>
+      <div className="section"><CardsManagement /></div>
+    </div>
   </div>
 )
 
