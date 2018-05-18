97066b99960fde569a56e43dee6750c452748b18
LucasLando <lucaslando1@gmail.com>
2018-05-18 13:03:01 -0300

    Improving css on lobby

--- src/components/GameLobby/GameLobby.jsx
+++ src/components/GameLobby/GameLobby.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './GameLobby.css'
 
 class GameLobby extends React.Component {
 
@@ -16,12 +17,16 @@ class GameLobby extends React.Component {
   render() {
     return (
       <div className="gameLobby">
-        <div>Game lobby</div>
-        <input type="text" name="playerName" placeholder="ingrese su nombre" onChange={this.handleChange}/>
-        
-        <button type="submit" name="submit" 
-          onClick={() => this.props.login(this.state.playerName)}>
-          Entrar</button>
+        <div className="title">
+          <h3>Timeline</h3>
+        </div>
+        <div className="login">
+          <input type="text" name="playerName" placeholder="ingrese su nombre" onChange={this.handleChange}/>
+          
+          <button type="submit" name="submit" 
+            onClick={() => this.props.login(this.state.playerName)}>
+            Entrar</button>
+        </div>
       </div>
     )
   }
