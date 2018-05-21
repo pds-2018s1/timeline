33c80c6b7e3e817d6bf857c19866b7fb035f566a
marcia-t <tejedamarcia@gmail.com>
2018-05-21 00:40:23 -0300

    testing game reducer

--- src/reducers/game.spec.js
+++ src/reducers/game.spec.js
@@ -1,5 +1,5 @@
 import {game} from './game'
-import {localAddCard as addCard} from '../actions/game'
+import {localAddCard as addCard, login, startGame} from '../actions/game'
 
 describe('game reducer', () => {
 
@@ -25,5 +25,70 @@ describe('game reducer', () => {
         cards: [{name: 'independencia de  ... ', year: '1987'  }]
     })
   })
+
+  it ('logs in a player', () => {
+      const action = login("Juan")
+      expect(game(undefined, action)).toEqual({
+        turn: "",
+        player: {
+         name: "Juan"
+        },
+        loggedIn: true,
+        opponents: [],
+        timeline: [],
+        gameStarted: false,
+        discard: {quantity: 0},
+        selectedCard: null,
+        winner: null,
+        administrate: false,
+        matchName: null, //TODO agrupar datos del juego en unico objeto
+        matchSize: null,
+        matches: [],
+        cards: []
+      })
+  })
+
+  /*it ('starts a game', async () => {
+      const state = {
+        turn: "",
+        player: {
+         name: "Juan"
+        },
+        loggedIn: true,
+        opponents: [],
+        timeline: [],
+        gameStarted: false,
+        discard: {quantity: 0},
+        selectedCard: null,
+        winner: null,
+        administrate: false,
+        matchName: null, 
+        matchSize: null,
+        matches: [],
+        cards: []
+      }
+      const action = startGame("partida 1", 3)
+      expect(game(state, action)).toEqual({
+        
+            turn: "",
+            player: {
+             name: "Juan",
+             playerHand: expect.any(Array)
+            },
+            loggedIn: true,
+            opponents: [],
+            timeline: expect.any(Array),
+            gameStarted: true,
+            discard: {quantity: 0},
+            selectedCard: null,
+            winner: null,
+            administrate: false,
+            matchName: "partida 1", 
+            matchSize: 3,
+            matches: [],
+            cards: expect.any(Array)
+           
+      })
+  })*/
 })
 
