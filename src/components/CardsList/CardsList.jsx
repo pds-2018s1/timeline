0db5602a41ed8670c34fc6a4d281d1181e05d4d9
marcia-t <tejedamarcia@gmail.com>
2018-05-22 02:20:12 -0300

    WIP: filtering cards

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -3,6 +3,11 @@ import List from 'material-ui/List'
 import CardsListItem from '../CardsListItem/CardsListItem'
 import Popup from "reactjs-popup";
 import './CardsList.css'
+import { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List'
+import IconButton from 'material-ui/IconButton'
+import DeleteIcon from 'material-ui-icons/Delete'
+
+
 
 class CardsList extends React.Component {
 
@@ -12,13 +17,28 @@ class CardsList extends React.Component {
       name: "",
       year: "",
       group: "",
-      img: ""
+      img: "",
+      filtered: false,
+      _cards: this.getInitialState
     }
   }
+
   componentDidMount() {
     const { fetchCards } = this.props
     fetchCards()
+    const {cards} = this.props
+    this.setState({_cards:cards, filtered: false})
   }
+  
+  getInitialState (){
+    const { fetchCards } = this.props
+    fetchCards()
+    const {cards} = this.props
+    console.log(cards)
+    return cards
+  }
+ 
+  
 
   handleNameChange = (event) =>
     this.setState({name: event.target.value})
@@ -35,14 +55,18 @@ class CardsList extends React.Component {
 
 
   render() {
-    const { cards } = this.props
-
+    let  {cards} = this.props
+    if (this.state.filtered){
+      cards = this.state._cards
+    }
+  
     return (
       <div>
        <h1>Listado de cartas</h1>
+       <input type="text" placeholder="Search" onChange={this.filterList}/>
         <List>
           {cards.map(card => (
-            <CardsListItem key={card._id} card={card} />
+              <CardsListItem key={card._id} card={card} />
           ))}
         </List>
         <Popup trigger={<button className="button"> Agregar una carta </button>} modal>
@@ -63,11 +87,7 @@ class CardsList extends React.Component {
         <input type="text" name="group" placeholder="Ingrese el grupo al que pertenece esta carta" onChange={this.handleGroupChange}/>
         
                 
-          <button
-            className="button"
-            onClick={() => {
-              this.addCard()
-              
+          <button className="button" onClick={() => {this.addCard()   
               close()
             }}
           >
@@ -84,7 +104,29 @@ class CardsList extends React.Component {
     const { addCardAction } = this.props
     addCardAction(this.state.name, this.state.year, this.state.img, this.state.group)
     this.setState({ name: '', year: '', img: '', group: '' })
+    const { cards } = this.props
+    this.setState({_cards:cards, filtered: false})
   } 
+
+  /*
+  deleteCard = (id) => {
+    const {deleteCardAction} = this.props
+    deleteCardAction(id)
+    const { cards } = this.props
+    this.setState({_cards:cards})
+  }*/
+
+
+  filterList = (event) => {
+    const _group = event.target.value
+    const { cards } = this.props
+    
+    const filteredCards = cards.filter(function (n){
+      return n.group.match(_group)
+      
+    })
+    this.setState({filtered: true,_cards: filteredCards})
+  }  
 }
 
 export default CardsList
