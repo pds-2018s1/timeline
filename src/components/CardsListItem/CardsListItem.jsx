0de07a4d0e1f3430e011fcf3a9729545c78ff942
marcia-t <tejedamarcia@gmail.com>
2018-05-22 17:50:06 -0300

    back to delete icon

--- src/components/CardsListItem/CardsListItem.jsx
+++ src/components/CardsListItem/CardsListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List'
 import IconButton from 'material-ui/IconButton'
-//import DeleteIcon from 'material-ui-icons/Delete'
+import DeleteIcon from 'material-ui-icons/Delete'
 import FaIconPack from 'react-icons/lib/fa'
 
 
@@ -20,8 +20,12 @@ class CardsListItem extends React.Component {
     <ListItem dense>
       <ListItemText primary={card.name+", "+card.year} secondary={card.group} />
       <ListItemSecondaryAction>
-        <IconButton aria-label="FaStopCircle"  onClick={this._handleDelete.bind(this, card._id)}>
+       
         
+        <IconButton aria-label="Delete">
+          <DeleteIcon onClick={this._handleDelete.bind(this, card._id)} />
+
+
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>
@@ -34,3 +38,4 @@ export default connect(undefined,
     deleteCard
   })
 )(CardsListItem)
+/* <IconButton aria-label="FaStopCircle"  onClick={this._handleDelete.bind(this, card._id)}>*/
