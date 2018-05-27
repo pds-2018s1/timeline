38fd57029ebd1708ce8b74cb8b39e2f0a8308623
LucasLando <lucaslando1@gmail.com>
2018-05-27 19:52:07 -0300

    Adding cypress and a first trivial test

--- src/storeCreator.js
+++ src/storeCreator.js
@@ -3,12 +3,18 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import {game as reducer} from './reducers/game'
 import thunk from 'redux-thunk';
 
+//Hack to avoid passing undefined to compose and making cypress tests to fail
+let devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if(! devtools ) {
+  devtools = a => a
+}
+
 export default function() {
   return createStore(
     reducer,
     compose( 
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      devtools
     )
   )
 }
