50cb5cce5bf1b830d068de85832a04df65b90141
LucasLando <lucaslando1@gmail.com>
2018-05-27 22:48:57 -0300

    Adding cypress tests for new match and adding validations to new match form (#33)

--- cypress/integration/timeline_spec.js
+++ cypress/integration/timeline_spec.js
@@ -1,9 +1,50 @@
 describe( 'timeline application', function(){
 
-  it ('should render homepage', function() {
-
+  it ('should render homepage', () => {
     cy.visit('http://localhost:3000')
+  })
 
+  it ('should take you to admin page after entering your name', () => {
+    cy.login()
+    cy.get('#greetingMessage').should('contain', 'Hola Lucas')
+  })
+
+  it ('should start a match after filling in the form', () => {
+    cy.login()
+    
+    cy.get('#newMatchName').type('New match')
+    cy.get('#newMatchSize').type('4')
+    cy.get('#newMatchSubmit').click()
+    
+    cy.get('#gameTable')
+    cy.get('#gameTimeline')
+    cy.get('#playerHand')
+  })
+
+  it ('should not let you start a new match without filling the form entirely', () => {
+    cy.login()
+    cy.get('#newMatchSubmit').should('be.disabled') 
+  })
+
+  it ('should not let you start a new match without typing the match name', () => {
+    cy.login()
+    cy.get('#newMatchSize').type('3')
+    cy.get('#newMatchSubmit').should('be.disabled') 
+  })
+
+  it ('should not let you start a new match without typing the match size', () => {
+    cy.login()
+    cy.get('#newMatchName').type('A match name')
+    cy.get('#newMatchSubmit').should('be.disabled') 
+  })
+
+  it ('should not let you start a new match whose size is lesser than 2 or greater than 8', () => {
+    cy.login()
+    cy.get('#newMatchSize').type('1')
+    cy.get('#newMatchSubmit').should('be.disabled') 
+
+    cy.get('#newMatchSize').type('9')
+    cy.get('#newMatchSubmit').should('be.disabled') 
   })
 
 })
--- src/components/CreateMatch/CreateMatch.jsx
+++ src/components/CreateMatch/CreateMatch.jsx
@@ -17,14 +17,18 @@ class CreateMatch extends React.Component {
   handleNumberChange = (event) =>
     this.setState({matchSize: event.target.value})
 
+  isFormValid = () => (
+    this.state.matchName && this.state.matchSize && this.state.matchSize > 1 && this.state.matchSize < 9
+  )
+
   render() {
     return (
       <div>
         <h3 className="createMatchTitle">Crear partida nueva</h3>
-        <input className="block newMatchInput" type="text" name="matchName" 
+        <input className="block newMatchInput" type="text" name="matchName" id="newMatchName"
           placeholder="Nombre de la partida" onChange={this.handleNameChange}/>
-        <input className="block newMatchInput" type="number" min="2" max="8" name="matchSize" onChange={this.handleNumberChange}/>
-        <button className="block createMatchButton" type="submit" name="submit"
+        <input className="block newMatchInput" type="number" min="2" max="8" name="matchSize" id="newMatchSize" onChange={this.handleNumberChange}/>
+        <button className="block createMatchButton" disabled={!this.isFormValid()} type="submit" name="submit" id="newMatchSubmit"
           onClick={() => this.props.startGame(this.state.matchName, this.state.matchSize)}>
           Crear partida
         </button>
