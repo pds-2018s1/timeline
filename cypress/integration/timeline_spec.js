51f4fcd5ec921bf30e74e2766df86345691c85e0
marcia-t <tejedamarcia@gmail.com>
2018-06-12 17:05:10 -0300

    adding commands and before actions

--- cypress/integration/timeline_spec.js
+++ cypress/integration/timeline_spec.js
@@ -1,47 +1,39 @@
 describe( 'timeline application', function(){
 
-  it ('should render homepage', () => {
+  beforeEach(function () {
     cy.visit('http://localhost:3000')
+    cy.login()
   })
 
-  it ('should take you to admin page after entering your name', () => {
-    cy.login()
-    cy.get('#greetingMessage').should('contain', 'Hola Lucas')
-  })
 
   it ('should start a match after filling in the form', () => {
-    cy.login()
     
-    cy.get('#newMatchName').type('New match')
-    cy.get('#newMatchSize').type('4')
-    cy.get('#newMatchSubmit').click()
-
-    cy.get('#waitingForMatch')
-    
-    cy.get('#gameTable', {timeout: 6000} ) //El cartel de waiting for match tiene un sleep de 3 segundos
+   
+    cy.newMatch()
+    cy.get('#gameTable', {timeout: 6000} )
     cy.get('#gameTimeline')
     cy.get('#playerHand')
   })
 
   it ('should not let you start a new match without filling the form entirely', () => {
-    cy.login()
+
     cy.get('#newMatchSubmit').should('be.disabled') 
   })
 
   it ('should not let you start a new match without typing the match name', () => {
-    cy.login()
+
     cy.get('#newMatchSize').type('3')
     cy.get('#newMatchSubmit').should('be.disabled') 
   })
 
   it ('should not let you start a new match without typing the match size', () => {
-    cy.login()
+
     cy.get('#newMatchName').type('A match name')
     cy.get('#newMatchSubmit').should('be.disabled') 
   })
 
   it ('should not let you start a new match whose size is lesser than 2 or greater than 8', () => {
-    cy.login()
+
     cy.get('#newMatchSize').type('1')
     cy.get('#newMatchSubmit').should('be.disabled') 
 
@@ -51,7 +43,7 @@ describe( 'timeline application', function(){
 
   it ('should let you get into a created match', () => {
    
-    cy.login()
+
     cy.get('.listOfMatches > :nth-child(1)').click()
     cy.get('#joinMatch').click()
     cy.get('#gameTable', {timeout: 6000} ) //El cartel de waiting for match tiene un sleep de 3 segundos
@@ -59,15 +51,10 @@ describe( 'timeline application', function(){
   })
 
 
-  it ('should start a match after filling in the form', () => {
-    cy.login()
-    
-    cy.get('#newMatchName').type('New match')
-    cy.get('#newMatchSize').type('4')
-    cy.get('#newMatchSubmit').click()
+  it ('should have make the game', () => {
 
-    cy.get('#waitingForMatch')
     
+     cy.newMatch()
     cy.get('#gameTable', {timeout: 6000} ) //El cartel de waiting for match tiene un sleep de 3 segundos
     cy.get('#gameTimeline')
     cy.get('#playerHand')
@@ -77,14 +64,9 @@ describe( 'timeline application', function(){
   })
 
   it ('should play a card and add it to the discard or the timeline', () => {
-    cy.login()
     
-    cy.get('#newMatchName').type('New match')
-    cy.get('#newMatchSize').type('4')
-    cy.get('#newMatchSubmit').click()
-
-    cy.get('#waitingForMatch')
     
+     cy.newMatch()
     cy.get('#gameTable', {timeout: 6000} )
     cy.get('#discardQuantity').should('contain', '0')
     cy.get('#cardsList > :nth-child(1)').click()
